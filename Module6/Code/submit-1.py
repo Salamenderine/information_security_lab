d6fdcb1b92a128170a6c7bc45f5009ca80af6f90
Haobo Li <haobli@ethz.ch>
2022-12-08 19:45:05 +0100

    task 1 completed

--- Module6/Code/submit-1.py
+++ Module6/Code/submit-1.py
@@ -2,97 +2,47 @@ import os
 import subprocess
 import time 
 
-def wipe_all_running():
+def terminate():
     os.system("pkill -9 node")
-    os.system("pkill -9 gdb")
-    time.sleep(2)
-    return 
+    os.system('pkill -9 gdb')
     
 def start_manager_and_peripheral():
-    os.system("cd /home/isl/t1  && /home/isl/t1/run_manager.sh ") 
-    os.system("cd /home/isl/t1  && /home/isl/t1/run_peripheral.sh ")
-    #os.system("pkill -9 string_parser")
-    return 
+    os.system("bash /home/isl/t1/run_manager.sh")
+    os.system("bash /home/isl/t1/run_peripheral.sh")
     
-def write(process,str):
-    string = (str + "\n").encode()
-    time.sleep(1)
-    process.stdin.write(string)
-    process.stdin.flush() 
-    return
+def gdb_cmd(gdb, cmd):
+    cmd = cmd + "\n"
+    cmd = cmd.encode()
+    gdb.stdin.write(cmd)
+    gdb.stdin.flush()
 
-def partOne(): 
-    gdb = subprocess.Popen(['gdb', '--args', 'python3', '/home/isl/t1/sp_server.py'], stdin=subprocess.PIPE) 
+def inject_message(msg):
+    gdb = subprocess.Popen(['gdb', '--args', 'python3', '/home/isl/t1/sp_server.py'], stdin=subprocess.PIPE)  
+    gdb_cmd(gdb, "add-symbol-file /home/isl/.local/lib/stringparser_core.so")
+    gdb_cmd(gdb, 'b gcm_crypt_and_tag')
+    gdb_cmd(gdb, 'r')
 
-    time.sleep(1) 
-    write(gdb,"add-symbol-file /home/isl/.local/lib/stringparser_core.so")
-    write(gdb,"set follow-fork-mode child")
-    write(gdb,"set breakpoint pending on")
-    write(gdb,"b gcm_crypt_and_tag")
-    write(gdb,"r")
-    time.sleep(1)
     os.system("node --no-warnings /home/isl/t1/remote_party  | tee /home/isl/t1/remote_party.log &")
-    write(gdb,"break gcm_crypt_and_tag")
-    write(gdb,"c")
-    write(gdb,"print input")
-    write(gdb,'set {char[40]} input = "<mes><action type=\\"key-update\\"/></mes>" ')
-    write(gdb,"print input")
-    write(gdb,"c 100")
+
+    gdb_cmd(gdb, 'break gcm_crypt_and_tag')
+    gdb_cmd(gdb, 'c')
+    gdb_cmd(gdb, msg)
+    gdb_cmd(gdb, 'c 100')
     time.sleep(2)
 
     gdb.terminate()
     os.system("pkill -9 gdb")
-    return 
+
+
+def main():
+    cmd1 = 'set {char[40]} input = "<mes><action type=\\"key-update\\"/></mes>"'
+    cmd2 = 'set {char[20]} input = "redeemToken, token"'
     
-def partTwo():
-    os.system("pkill -9 gdb")
-    os.system("pkill -9 string_parser")
-    gdb = subprocess.Popen(["gdb", "screen"], stdin=subprocess.PIPE) #/home/isl/t1/string_parser"],
-    time.sleep(1)
-    write(gdb,"set follow-fork-mode child")
-    write(gdb,"set breakpoint pending on")
-    write(gdb,"break stringParser")
-    write(gdb,"run -dmS string_parser /home/isl/t1/string_parser")
-    #write(gdb,"set detach-on-fork off")
-    #write(gdb,"set follow-fork-mode child")
-   
-    write(gdb,"break *0x040338a")
-    write(gdb,"continue")
-    #write(gdb,"run")
-    time.sleep(1)
-    os.system("node --no-warnings /home/isl/t1/remote_party | tee /home/isl/t1/remote_party.log &")
-    time.sleep(1)
-    write(gdb,"set redirectAdmin = 0x8da8a1")
-    write(gdb,"break *0x04033de")
-    write(gdb,"continue")
-    write(gdb,"set $rax = redeemer[2]")
-    write(gdb,"break *0x4033fb")
-    write(gdb,"continue")
-    write(gdb,"set $al = 0x13")
-    write(gdb,"continue")
-    time.sleep(2)
-#    print("MANAGER LOGS")
-#    print("===============================")        
-#    os.system("cat /home/isl/t1/manager.log")
-#    print("===============================")    
-#   print("PERIPHERAL LOGS")
-#    print("===============================")    
-#    os.system("cat /home/isl/t1/peripheral.log")
-#    print("===============================")    
-#    print("TERMINATING GDB")
-    gdb.terminate()
-    os.system("pkill -9 gdb")	
-    return 
+    terminate()
+    start_manager_and_peripheral()
+    inject_message(cmd1)
+    inject_message(cmd2)
+    terminate()
 
-
-
-wipe_all_running()
-start_manager_and_peripheral()
-partOne() 
-
-###Following are just needed to make function return nicely. 
-#partTwo()
-#time.sleep(100)
-#print("TERMINATING")
-wipe_all_running()
-os.system("cd /home/isl/t1 && /home/isl/t1/run.sh")
+if __name__ == "__main__":
+    main()
